16c88c2308e89ac2fdeee6e39001f43481784126
Eduardo <edu.mangaba@gmail.com>
2019-10-18 14:54:32 -0300

    aprender kmeans

---
+++ K-means/cluster.R
@@ -0,0 +1,6 @@
+install.packages("factoextra")
+posicionamentos <- read.csv("(VLIMPA)posicionamentos.csv")
+posicionamentos$coligações = NULL
+posicionamentos$Orientação = NULL
+posicionamentos$partido = NULL
+
